a235e760b2c05159392ae4a4ee14ce06da621c68
Erwan RAHIM <waren78.krahim@gmail.com>
2024-02-08 17:11:09 +0100

    finished list index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_154912) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_122754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_154912) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "movies", force: :cascade do |t|
